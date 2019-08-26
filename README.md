c46355548b8a3589afdf23da9e07cfa66b7160a8
huangbiao <huangbiao@npnets.com>
2019-08-26 13:57:58 +0800

    first

---
+++ README.md
@@ -0,0 +1,12 @@
+## Mius
+mius上次更新文件到腾讯cos
+
+#### 使用方法
+
+``` bash
+#  安装依赖
+npm install || yarn
+
+# 本地运行
+npm run dev || yarn run update
+```
---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+    SecretId: 'AKIDTp5KPII6x9HMK8wZ8aqEXhgaGlAOkBnE',
+    SecretKey: 'UqQrDapunZ6ThpgQ1QhbhruCLLTXZUGx',
+    Bucket: 'mius-1252282306',
+    Region: 'ap-shanghai'
+};
+
+module.exports = config;
